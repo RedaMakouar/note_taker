e42f87dd6f4b64857be80df4e0c21018f72df126
reda makouar <makouarreda@gmail.com>
2020-05-17 15:44:42 +0000

    Updates

---
+++ precache-manifest.185436c885b5e5356c9943eb1fcc5dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7415bfe4aafb34b8605d35eb08d28b3",
+    "url": "/note_taker/index.html"
+  },
+  {
+    "revision": "9a5d3d205d14cadddeb8",
+    "url": "/note_taker/static/js/2.93786c98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/note_taker/static/js/2.93786c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2371c634648414330bf4",
+    "url": "/note_taker/static/js/main.7416d24f.chunk.js"
+  },
+  {
+    "revision": "546105854faf52d8456e",
+    "url": "/note_taker/static/js/runtime-main.b62cf597.js"
+  }
+]);
